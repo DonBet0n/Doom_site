a240eac0b6ffa9c125c0686b6ffe8c5c19fdb8f4
DonBet0n <mr.kostushevich@gmail.com>
2023-04-26 00:28:17 +0300

    headr demo 1.0

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css\style.css" rel="stylesheet">
+    <link type="image/x-icon" href="img\doomicon512.ico" rel="shortcut icon">
+    <title>Doom In</title>
+</head>
+<body>
+    <header id="start">
+        <div class="header__wrapper">
+        <!-- <video class="bckvideo" width="100%" autoplay muted loop id="MyVideo">
+                <source src="img\bckvideo.mp4" type="video/mp4">
+        </video> -->
+        <img class="bck" src="img\bck_description.png" >
+            <div class="line_top" id="topline">
+                <div class="logo"><img src="img\logo.svg" alt=""></div>
+                <div class="name_site">Doom</div>
+                <div class="profile"><a href="#"><img src="img\profile.svg"></a></div>
+            </div>
+            <div class="header__flexblock-column">
+                <div class="flexblock">
+                    <ul>
+                        <li class="li_flexblock"><a href="#start">Вступ</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#evaluations">опис</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#">палач рока</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#">демони</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#">зброя</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="https://store.steampowered.com/app/782330/DOOM_Eternal/">сторiнка В STEAM</a><div class="line"></div></li>
+                    </ul>
+                </div>
+            </div>
+
+           
+                     
+
+
+        </div>
+</header> 
+<div id="evaluations" class="tab_description">
+
+        <div class="description_content">
+            <h2 class="text">Вступ</h2>
+            
+        </div>
+</div>
+</body>
+</html>
