116eb3be8e566ce9618b6207d71dd98f739420d3
DonBet0n <mr.kostushevich@gmail.com>
2023-04-27 00:30:59 +0300

    demo site 10

--- index.php
+++ index.php
@@ -24,10 +24,10 @@
                 <div class="flexblock">
                     <ul>
                         <li class="li_flexblock"><a href="#start">Вступ</a><div class="line"></div></li>
-                        <li class="li_flexblock"><a href="#evaluations">опис</a><div class="line"></div></li>
-                        <li class="li_flexblock"><a href="#">палач рока</a><div class="line"></div></li>
-                        <li class="li_flexblock"><a href="#">демони</a><div class="line"></div></li>
-                        <li class="li_flexblock"><a href="#">зброя</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#tab_description">опис</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#tab_kat_roka">Кат року</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#tab_demons">демони</a><div class="line"></div></li>
+                        <li class="li_flexblock"><a href="#tab_guns">зброя</a><div class="line"></div></li>
                         <li class="li_flexblock"><a href="https://store.steampowered.com/app/782330/DOOM_Eternal/">сторiнка В STEAM</a><div class="line"></div></li>
                     </ul>
                 </div>
@@ -67,12 +67,129 @@
 
         </div>
 </header> 
-<div id="evaluations" class="tab_description">
+<main>
+    <div class="main__wrapper">
+       
+            <div id="tab_description" class="tab_description">
+
+                    <div class="description_content">
+                        <h2 class="text">Вступ</h2>
+                            <div class="flex_derec">
+                                <div class="number">1</div>
+                                <div class="description_content_text">
+                                    DOOM Eternal - гра в жанрі шутер від першої особи,
+                                    розроблена компанією Id Software і є сіквелом DOOM.
+                                    Протистояння Кату Року і сил пекла триває,
+                                    тепер вам доведеться звільнити Землю від 
+                                    демонічної загрози, переміщатися за вимірами
+                                    та знищувати як нових істот у всесвіті Дум,
+                                    так і добре знайомих демонів із потужною
+                                    зброєю та здібностями.
+                                </div>
+                            </div>
+                </div> 
+            </div>
+
+        
+
+        <div id="tab_kat_roka" class="tab_kat_roka">
+        <div class="description_content">
+            <h2 class="text">Кат Року</h2>
+            <div class="flex_derec">
+                <div class="number">2</div>
+                    <div class="tab_kat_text">
+                        Палач Рока - колишній космічний десантник, який став великим 
+                        воїном Нічних Стражів. Він почав свій хрестовий
+                        похід проти демонів після того, як вони напали
+                        на Фобос і Деймос де він служив.
+                        Він пройшов через Ад, знищуючи всіх демонів на
+                        своєму шляху, і потрапив у вимір Аргент Д’Нур,
+                        де він знайшов нових союзників і нову броню.
+                        Він був запечатаний у саркофазі демонами,
+                        але потім був звільнений ОАК на Марсі.
+                        Він продовжив свою боротьбу з демонами 
+                        на Марсі, Землі і в інших вимірах.
+                        Він має надзвичайно високий рівень
+                        фізичної сили, швидкості, витривалості
+                        і інтелекту, а також доступ до розмаїття
+                        потужної зброї і технологій
+                        </div>
+            </div>
+
+        </div>
+        </div> 
+
+
+        <div id="tab_demons" class="tab_demons">
 
         <div class="description_content">
-            <h2 class="text">Вступ</h2>
-            
+            <h2 class="text">Демони</h2>
+                <div class="flex_derec">
+                    <div class="number">3</div>
+                    <div class="demons_text">
+                        Демони - це ті з ким ви будете стикатись найчастіше.
+                        <p class="demons_text">Демони, Імп, Зомбі, Арахнотрон, Солдат, Солдат із щитом,
+                        Горгулья, Механічний зомбі, Рижучий Манкубус, Втрачена душа, Туша,
+                        Рицар пекла, Пліть, Какодемон, Кіберманкубус, Елементаль болю, 
+                        Ревенант, Пінки, Примара, Лицар жаху, Мародер, Барон пекла, Архвіль, Тиран</p>
+                    </div>
+                </div>
+        </div> 
         </div>
-</div>
+
+
+        <div id="tab_guns" class="tab_guns">
+
+        <div class="description_content">
+            <h2 class="text">Зброя</h2>
+                <div class="flex_derec">
+                    <div class="number">4</div>
+                    <div class="guns_text">
+                        <!-- shotgun -->
+                    <div class="conteiner_guns">
+                        <div class="shotgun gun_flex">
+                            <img class="img_shotgun"  src="img\shotgun.png" alt="">
+                            <!-- <div class="name_gun">Дробовик</div> -->
+                            <div class="discr_guns">Супердробовик - являє собою 
+                                переломну двоствольну обрізану рушницю.
+                                 Потужніший за бойовий дробовик, але потребує 
+                                 частого перезарядження через невелику ємність.</div>
+                         </div>
+                        <!-- bfg-9000 -->
+                         <div class="bfg-9000 gun_flex">
+                            <img class="img_bfg-9000" src="img\bfg-9000.png" alt="">
+                            <!-- <div class="name_gun">BFG-9000</div> -->
+                            <div class="discr_guns">BFG (Біотична Силова Гармата) - 
+                                BFG, велика металева гармата, веде вогонь зеленими 
+                                кулями плазми (40 осередків на вибух). Для загальних 
+                                цілей, її можна вважати найпотужнішою зброєю в грі.</div>
+                         </div>
+                         <!-- gornilo -->
+                         <div class="gornilo gun_flex">
+                            <img class="img_gornilo" src="img\gornilo.png" alt="">
+                            <!-- <div class="name_gun">Горнило</div> -->
+                            <div class="discr_guns">Горнило Ката - це артефакт
+                                 і наймогутніша зброя в грі DOOM Eternal,
+                                  клинок, здатний убити майже будь-якого 
+                                  демона з одного удару.</div>
+                         </div>
+
+                    </div>
+                        
+                    </div>
+                </div>
+        </div> 
+        </div>
+
+
+
+
+
+
+    </div>
+</main>
+<footer class="footer">
+
+</footer>
 </body>
 </html>
